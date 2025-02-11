3e66f29ece01f1b0196df9ac75c4f6d4a202374b
Sergio del Amo <sergio.delamo@softamo.com>
2025-02-11 10:24:01 +0100

    add @Produces plain/txt

--- permit-all-body-issue/src/main/java/com/example/ExampleController.java
+++ permit-all-body-issue/src/main/java/com/example/ExampleController.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Produces;
 import jakarta.annotation.security.PermitAll;
 
 @Controller
@@ -13,18 +15,21 @@ public class ExampleController implements ExampleApi {
 
     @Override
     @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
     public String withQuery(String query) {
         return "I got a query yay";
     }
 
     @Override
     @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
     public String withHeader(String header) {
         return "I got a header yay";
     }
 
     @Override
     @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
     public String withBody(String body) {
         return "I got a body yay";
     }
